2378b3b50a8559547a6042f76a456da350cae938
reezkaal <133508876+reezkaal@users.noreply.github.com>
2024-04-02 05:56:52 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php 
+	include 'index.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Selamat datang di Kedai Kanzoe, sebuah destinasi kue yang berlokasi di jantung kota Tangerang, di mana aroma manis dan keramahan bertemu dalam sebuah pengalaman yang tak terlupakan. Sejak kami membuka pintu untuk pertama kalinya, kami telah berusaha untuk menjadi lebih dari sekadar toko kue biasa; kami adalah tempat di mana komunitas berkumpul, dan setiap kue adalah karya seni yang mewakili cinta dan dedikasi kami.
+    Dipandu oleh semangat untuk memberikan yang terbaik kepada pelanggan kami, kami telah menempatkan standar kualitas yang tinggi dalam setiap produk yang kami sajikan. Dengan memilih bahan-bahan segar terbaik dan menggunakan teknik pembuatan kue yang cermat, kami berkomitmen untuk menciptakan kue-kue yang tidak hanya lezat, tetapi juga memberikan kebahagiaan kepada setiap pelanggan.
+
+</p>
+<p>
+Kami, di Kedai Kanzoe, bangga akan warisan kue tradisional, sementara kami juga selalu mencari cara untuk menghadirkan inovasi dan keunikan dalam setiap kreasi kami. Dari kue ulang tahun yang dipersonalisasi hingga kue-kue klasik yang merayakan momen-momen istimewa, kami memiliki beragam pilihan untuk memenuhi semua selera dan kebutuhan.
+Namun, di balik setiap karya seni yang kami hasilkan, terdapat cerita tentang semangat dan dedikasi tim kami yang berbakat. Kami adalah keluarga di Kedai Kanzoe, yang bersatu dalam tujuan bersama untuk memberikan yang terbaik bagi pelanggan kami di Tangerang dan sekitarnya.</p>
+</div>
