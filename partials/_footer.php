b20eaab7c6dfcdeefd57d4f3fe12f7ec0280eb84
reezkaal <133508876+reezkaal@users.noreply.github.com>
2024-06-10 06:32:03 +0700

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Designed by <a href="https://github.com/rezkaal" target="_blank" rel="noopener noreferrer">Kelompok B</a></p>
+</div>
