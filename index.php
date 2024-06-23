7fd3c09a8bb882537a57954ebefb35a3222cc250
reezkaal <133508876+reezkaal@users.noreply.github.com>
2024-06-24 02:02:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,97 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php
+session_start();
+include 'koneksi.php';
+?>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <title>Home</title>
-    <link rel = "icon" href ="img/logotoko.jpg" type = "image/x-icon">
-  </head>
-<body>
-  <?php include 'partials/_dbconnect.php';?>
-  <?php require 'partials/_nav.php' ?>
-  
-  <!-- Category container starts here -->
-  <div class="container my-3 mb-5">
-    <div class="col-lg-2 text-center bg-light my-3" style="margin:auto;border-top: 2px groove black;border-bottom: 2px groove black;">     
-      <h2 class="text-center">KATEGORI MENU</h2>
-    </div>
-    <div class="row">
-      <!-- Fetch all the categories and use a loop to iterate through categories -->
-      <?php 
-        $sql = "SELECT * FROM `categories`"; 
-        $result = mysqli_query($conn, $sql);
-        while($row = mysqli_fetch_assoc($result)){
-          $id = $row['categorieId'];
-          $cat = $row['categorieName'];
-          $desc = $row['categorieDesc'];
-          echo '<div class="col-xs-3 col-sm-3 col-md-3">
-                  <div class="card" style="width: 18rem;">
-                    <img src="img/kategori-'.$id. '.jpg" class="card-img-top" alt="image for this category" width="249px" height="270px">
-                    <div class="card-body">
-                      <h5 class="card-title"><a href="viewKueList.php?catid=' . $id . '">' . $cat . '</a></h5>
-                      <p class="card-text">' . substr($desc, 0, 30). '... </p>
-                      <a href="viewKueList.php?catid=' . $id . '" class="btn btn-primary">View All</a>
+<?php include 'header.php'; ?>
+<div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="owl-carousel header-carousel position-relative">
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" style="height: 800px;object-fit: cover;" src="home/img/kk1.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-lg-8">
+                                <p class="text-primary text-uppercase fw-bold mb-2">Selamat Datang Di Website</p>
+                                <h1 class="display-1 text-light mb-4 animated slideInDown">Kedai Kanzoe</h1>
+                                <p class="text-light fs-5 mb-4 pb-3">Harga Kaki Lima, Kualitas Bintang Lima</p>
+                                <a href="produk.php" class="btn btn-primary rounded-pill py-3 px-5">Menu</a>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                </div>';
-        }
-      ?>
+                </div>
+            </div>
+            <div class="owl-carousel-item position-relative">
+                <img class="img-fluid" style="height: 800px;object-fit: cover;" src="home/img/kk2.jpg" alt="">
+                <div class="owl-carousel-inner">
+                    <div class="container">
+                        <div class="row justify-content-start">
+                            <div class="col-lg-8">
+                               <p class="text-primary text-uppercase fw-bold mb-2">Selamat Datang Di Website</p>
+                                <h1 class="display-1 text-light mb-4 animated slideInDown">Kedai Kanzoe</h1>
+                                <p class="text-light fs-5 mb-4 pb-3">Harga Kaki Lima, Kualitas Bintang Lima</p>
+                                <a href="produk.php" class="btn btn-primary rounded-pill py-3 px-5">Menu</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 
+    <div class="container-xxl py-6">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="row img-twice position-relative h-100">
+                        <div class="col-6">
+                            <img class="img-fluid rounded" style="height: 200px;object-fit: cover;" src="home/img/kk3.jpg" alt="">
+                        </div>
+                        <div class="col-6 align-self-end">
+                            <img class="img-fluid rounded" style="height: 200px;object-fit: cover;" src="home/img/kk4.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                    <div class="h-100">
+                        <p class="text-primary text-uppercase mb-2">Tentang</p>
+                        <h1 class="display-6 mb-4">Kedai Kanzoe</h1>
+                        <p>Kedai Kanzoe merupakan usaha keluarga yang dirintis mulai dari menitipkan kue kue dari warung ke warung lain sampai membuka toko sendiri, yang karyawan nya terdiri dari semua anggota keluarga. Walaupun hanya memiliki karyawan yang beranggota keluarga inti Kedai Kanzoe mampu melayani berbagai pesanan pelanggan serta mengoperasikan toko dengan maksimal.</p>
+                        <p>Kue tradisional adalah makanan kecil dimana resepnya diwariskan secara turun- temurun. Jenis kue tradisional terbagi dua yaitu kue tradisional kering dan kue tradisonal basah.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    <?php require 'partials/_footer.php' ?>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>         
-    <script src="https://unpkg.com/bootstrap-show-password@1.2.1/dist/bootstrap-show-password.min.js"></script>
-</body>
-</html>
+    <div class="container-xxl py-6">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
+                <p class="text-primary text-uppercase mb-2">Menu</p>
+                <h1 class="display-6 mb-4">Menu</h1>
+            </div>
+            <div class="row g-4">
+				<?php $ambil = $koneksi->query("SELECT *FROM produk LEFT JOIN kategori ON produk.idkategori=kategori.idkategori order by idproduk desc limit 3"); ?>
+			<?php while ($perproduk = $ambil->fetch_assoc()) { ?>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <div class="team-item text-center rounded overflow-hidden">
+                        <img class="img-fluid" src="foto/<?php echo $perproduk['fotoproduk'] ?>" alt="">
+                        <div class="team-text">
+                            <div class="team-title">
+                                <h5><?php echo $perproduk["namaproduk"] ?></h5>
+                                <span>Rp <?php echo number_format($perproduk['hargaproduk']) ?></span>
+                            </div>
+                            <div class="team-social">
+                                <a href="detail.php?id=<?php echo $perproduk['idproduk']; ?>" class="btn btn-primary rounded-pill py-3 px-5 w-100">Pesan</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               <?php } ?> 
+            </div>
+        </div>
+    </div>
+
+<?php
+include 'footer.php';
+?>
